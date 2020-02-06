3c5eaa05d3bccf60bd8e31c01c1287862400aee9
Rick <rdsavage10@gmail.com>
2020-02-06 17:53:39 -0500

    merging

--- src/game.js
+++ src/game.js
@@ -187,10 +187,6 @@ Piece.prototype.collision = function(x, y, piece) {
 
 
 
-document.addEventListener('keydown', CONTROL);
-
-
-
 function drop() {
 
   let now = Date.now();
@@ -206,18 +202,24 @@ function drop() {
   }
 }
 
+document.addEventListener('keydown', CONTROL);
+
 function CONTROL(event) {
 
   if (event.keyCode === 37) {
+    event.preventDefault();
     p.moveLeft();
     dropStart = Date.now();
   } else if (event.keyCode === 38) {
+    event.preventDefault();
     p.rotate();
     dropStart = Date.now();
   } else if (event.keyCode === 39) {
+    event.preventDefault();
     p.moveRight();
     dropStart = Date.now();
   } else if (event.keyCode === 40) {
+    event.preventDefault();
     p.moveDown();
   }
 }
