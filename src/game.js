410ea42f3175dbf940931dc9bb48fff6da80409a
Elliott Woodward <elliottcwoodward@gmail.com>
2020-02-08 09:57:39 -0500

    Fix issue #1

--- src/game.js
+++ src/game.js
@@ -235,15 +235,12 @@ function CONTROL(event) {
   if (event.keyCode === 37) {
     event.preventDefault();
     p.moveLeft();
-    dropStart = Date.now();
   } else if (event.keyCode === 38) {
     event.preventDefault();
     p.rotate();
-    dropStart = Date.now();
   } else if (event.keyCode === 39) {
     event.preventDefault();
     p.moveRight();
-    dropStart = Date.now();
   } else if (event.keyCode === 40) {
     event.preventDefault();
     p.moveDown();
